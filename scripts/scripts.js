076d00da7c0c4bfae9414d56f8a1e8ac403bb777
Thor Davis <36608610+mrthordavis@users.noreply.github.com>
2026-02-12 16:31:25 +0100

    Changes to madklub.html

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -7,3 +7,31 @@ function myFunction() {
         document.getElementById("password").placeholder = "Try again";
     }
 }
+
+const spreadsheetID = "1GYe7UtTsZEgB4V4kOd_hTGG1DAnmLLK3P6xsl7uJN3o";
+
+const apiKey = "AIzaSyD9FBj8frnBTB4dNa_RLFmEIEphsDTZVJ0";
+
+const range = encodeURIComponent("'Februar 26'!A11:C31");
+
+const url = `https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetID}/values/${range}?key=${apiKey}`;
+
+//`https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetId}/values/Sheet1?key=${apiKey}`;
+
+async function readSheet() {
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+
+        const date = new Date();
+        const day = date.getDate();
+
+        const row = data.values.find(row => row[0]?.match(/d\. (\d+)/)?.[1] == day); //Tak Chat!
+
+        document.getElementById("date").textContent = "I dag " + row[0] + " bliver der serveret:"
+        document.getElementById("food").textContent = row[2];
+        document.getElementById("chef").textContent = "Kok: " + row[1];
+    } catch (err) {
+        document.getElementById("food").textContent = "Something went wrong";
+    }
+}
